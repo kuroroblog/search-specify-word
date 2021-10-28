b65037a07df4da12655119afa034569292390a8b
tocchimittsu <arimagedon21@gmail.com>
2021-10-29 08:59:36 +0900

    修正

---
+++ main.py
@@ -0,0 +1,20 @@
+# ファイルを開く。
+# 参考 : https://note.nkmk.me/python-file-io-open-with/
+fp = open('sample.txt', "r", encoding="utf-8")
+
+# 特定の文字列を設定する。hogeとする。
+specify_word = 'hoge'
+
+# ファイルをリスト形式で読み込む。
+# sample.txtの場合、['hoge', 'fuga', 'piyo', 'aiueo', 'kakikukeko', 'hogepiyo']で読み込む。
+# 参考 : https://note.nkmk.me/python-file-io-open-with/
+# for文を回すことで、wordの中へ順に'hoge', 'fuga', ...が格納される。
+for word in fp.readlines():
+    # strip : hogeの前後に含まれる、空白文字を削除する。
+    # 参考 : https://note.nkmk.me/python-str-remove-strip/
+    word = word.strip()
+
+    # 特定の文字列が一致する、wordを探し出す。
+    # 参考 : https://note.nkmk.me/python-in-basic/
+    if specify_word in word:
+        print(word)
